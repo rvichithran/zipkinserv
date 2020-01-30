286bebd105a8e031b1be54f437d41292612a31b4
Ratish Vichithran <ratish.vichithran@gmail.com>
2020-01-30 17:52:27 -0500

    Initial Commit

---
+++ src/main/java/open/zipkin/trace/zipkinserv/ZipkinservApplication.java
@@ -0,0 +1,14 @@
+package open.zipkin.trace.zipkinserv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ZipkinservApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZipkinservApplication.class, args);
+    }
+
+}
---
+++ src/test/java/open/zipkin/trace/zipkinserv/ZipkinservApplicationTests.java
@@ -0,0 +1,13 @@
+package open.zipkin.trace.zipkinserv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZipkinservApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
